ca79f441fd267f7728c9c2563cd7a3b7bed53d01
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-08-21 08:52:02 +0900

    test article.html

--- InfinitySpirit/script/convert.py
+++ InfinitySpirit/script/convert.py
@@ -35,7 +35,7 @@ with open("./InfinitySpirit/template/index.html") as f:
     find_elem("InfinitySpiritThumbnail", "thumbnail")
 
 
-def mdc(markdown_text):
+def mdc(markdown_text,type="markdown"):
     extensions = [
         "extra",
         "admonition",
@@ -62,22 +62,23 @@ def mdc(markdown_text):
     convert_mode = {"~~": False}
     for markdown_line in markdown_text.split("\n"):
         # ~~を変換する機能がないので自分で実装する
-        while "~~" in markdown_line:
-            if convert_mode["~~"]:
-                markdown_line = markdown_line.replace("~~", "</s>", 1)
-            else:
-                markdown_line = markdown_line.replace("~~", "<s>", 1)
-            convert_mode["~~"] = not convert_mode["~~"]
-        if markdown_line.startswith("# title: "):
-            markdown_title = markdown_line[9:]
-        elif markdown_line.startswith("# ") and markdown_title == "":
-            markdown_title = markdown_line[2:]
-            markdown_result += markdown_line + "\n"
-        elif markdown_line.startswith("<h1>") and markdown_title == "":
+        if type=="markdown":
+            while "~~" in markdown_line:
+                if convert_mode["~~"]:
+                    markdown_line = markdown_line.replace("~~", "</s>", 1)
+                else:
+                    markdown_line = markdown_line.replace("~~", "<s>", 1)
+                convert_mode["~~"] = not convert_mode["~~"]
+            if markdown_line.startswith("# title: "):
+                markdown_title = markdown_line[9:]
+            elif markdown_line.startswith("# ") and markdown_title == "":
+                markdown_title = markdown_line[2:]
+                markdown_result += markdown_line + "\n"
+            elif markdown_line.startswith("# date: "):
+                markdown_date = markdown_line[8:]
+        if markdown_line.startswith("<h1>") and markdown_title == "":
             markdown_title = markdown_line.replace("<h1>", "").replace("</h1>", "")
             markdown_result += markdown_line + "\n"
-        elif markdown_line.startswith("# date: "):
-            markdown_date = markdown_line[8:]
         elif markdown_line.startswith("<date>"):
             markdown_date = (
                 markdown_line.replace("<date>", "")
@@ -119,53 +120,59 @@ def convert(date, now_year, indent) -> None:
         for article_dir in article_dirs:
             print(article_dir)
             markdown_path = "./" + month_dir + "/" + article_dir + "/article.md"
-            if os.path.isfile(markdown_path):
-                with open(markdown_path) as f:
-                    converted = mdc(f.read())
-                    base_html = converted["html"]
-                    article_title = converted["title"]
-                    article_date = converted["date"]
-                    print("article id:", article_dir)
-                    print("article title:", article_title)
-                    print("article date:", article_date)
-                    export_html = template_html
-                    export_html = export_html.replace(
-                        replace_pos["title"], article_title
-                    )
-                    base_html = (
-                        indent_html(base_html, indent)
-                        + "\n"
-                        + indent * " "
-                        + "</InfinitySpiritContent>"
-                    )
-                    export_html = export_html.replace(
-                        replace_pos["content"], "<InfinitySpiritContent>\n" + base_html
-                    )
-                    export_html = export_html.replace(
-                        replace_pos["date"],
-                        "<InfinitySpiritDate>" + article_date + "</InfinitySpiritDate>",
-                    )
-                    article_thumbnail = ""
-                    for file_name in search.files("./" + month_dir + "/" + article_dir):
-                        if file_name.startswith("thumbnail"):
-                            article_thumbnail = file_name
-                    export_html = export_html.replace(
-                        replace_pos["thumbnail"], 
-                        "https://the-infinitys.f5.si/article-2024/" + 
-                        month_dir + "/" + article_dir + "/" + article_thumbnail
-                    )
-                    with open(
-                        "./" + month_dir + "/" + article_dir + "/index.html", mode="w"
-                    ) as index_html:
-                        index_html.write(export_html)
-                    article_index_list.append(
-                        {
-                            "id": article_dir,
-                            "title": article_title,
-                            "date": article_date,
-                            "thumbnail": article_thumbnail,
-                        }
-                    )
+            html_path = "./" + month_dir + "/" + article_dir + "/article.html"
+            if os.path.isfile(markdown_path) or os.path.isfile(html_path):
+                converted=""
+                if os.path.isfile(markdown_path):
+                    with open(markdown_path) as f:
+                        converted = mdc(f.read())
+                else:
+                    with open(html_path) as f:
+                        converted = mdc(f.read(),type="html")
+                base_html = converted["html"]
+                article_title = converted["title"]
+                article_date = converted["date"]
+                print("article id:", article_dir)
+                print("article title:", article_title)
+                print("article date:", article_date)
+                export_html = template_html
+                export_html = export_html.replace(
+                    replace_pos["title"], article_title
+                )
+                base_html = (
+                    indent_html(base_html, indent)
+                    + "\n"
+                    + indent * " "
+                    + "</InfinitySpiritContent>"
+                )
+                export_html = export_html.replace(
+                    replace_pos["content"], "<InfinitySpiritContent>\n" + base_html
+                )
+                export_html = export_html.replace(
+                    replace_pos["date"],
+                    "<InfinitySpiritDate>" + article_date + "</InfinitySpiritDate>",
+                )
+                article_thumbnail = ""
+                for file_name in search.files("./" + month_dir + "/" + article_dir):
+                    if file_name.startswith("thumbnail"):
+                        article_thumbnail = file_name
+                export_html = export_html.replace(
+                    replace_pos["thumbnail"], 
+                    "https://the-infinitys.f5.si/article-2024/" + 
+                    month_dir + "/" + article_dir + "/" + article_thumbnail
+                )
+                with open(
+                    "./" + month_dir + "/" + article_dir + "/index.html", mode="w"
+                ) as index_html:
+                    index_html.write(export_html)
+                article_index_list.append(
+                    {
+                        "id": article_dir,
+                        "title": article_title,
+                        "date": article_date,
+                        "thumbnail": article_thumbnail,
+                    }
+                )
 
         def get_date(obj) -> str:
             return obj["date"]
