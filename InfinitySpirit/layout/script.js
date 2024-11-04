55cb0730cd3083d6e6874cd6d21fd2fb0d80a0af
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-11-04 05:17:41 +0000

    変更がエグいことになりそう

--- InfinitySpirit/layout/script.js
+++ InfinitySpirit/layout/script.js
@@ -207,19 +207,3 @@ const shareX = () => {
   window.open((url = X_url), (target = "_blank"));
 };
 
-// 楽曲投稿に使うやつです。
-const generate_audio_player = () => {
-  const inf_audio_elem = document.querySelector("inf-audio");
-  if (inf_audio_elem == null) {
-    return;
-  }
-  const audio_player = document.createElement("iframe");
-  const result_url = `/app/audio-player/index.html?audio=${encodeURI(
-    inf_audio_elem.getAttribute("data-audio")
-  )}&img=${encodeURI(
-    inf_audio_elem.getAttribute("data-img")
-  )}&title=${encodeURI(inf_audio_elem.getAttribute("data-title"))}`;
-  audio_player.src = result_url;
-  inf_audio_elem.append(audio_player);
-};
-generate_audio_player();
